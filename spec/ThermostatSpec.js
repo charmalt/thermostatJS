6da1bf7222021657490f5f308b933edae31897f4
Charlene <charlenechetcuti@gmail.com>
2018-05-16 14:15:55 +0100

    check for min and max temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -17,12 +17,34 @@ describe("up", function() {
     thermostat.up();
     expect(thermostat.temperature).toEqual(DEFAULTTEMP + INCREASE);
   });
+
+  describe("power saving on", function(){
+    it("should raise error if above maximum temperature (25)", function(){
+      for(i=1; i<=5; i++){ thermostat.up(); };
+      expect(function(){
+        thermostat.up();
+      }).toThrowError('Maximum Temperature!');
+    });
+  });
+  
 });
 
 describe("down", function() {
   it("should decrease the temperature by specified amount", function() {
     thermostat.down();
     expect(thermostat.temperature).toEqual(DEFAULTTEMP - DECREASE);
+  });
+  it("should raise error if below minimum temperature", function(){
+    for(i=1; i<=10; i++){ thermostat.down(); };
+    expect(function(){
+      thermostat.down();
+    }).toThrowError("Minimum Temperature!");
+  });
+});
+
+describe("power saving mode", function(){
+  it("should be on by default", function(){
+    expect(thermostat.powerSaving).toEqual(true);
   });
 });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,15 +1,24 @@
 var DEFAULTTEMP = 20;
 var INCREASE = 1;
 var DECREASE = 1;
+var MINIMUM = 10;
+var MAXIMUM = 25;
 
 function Thermostat() {
     this.temperature = DEFAULTTEMP;
+    this.powerSaving = true;
 }
 
 Thermostat.prototype.up = function() {
+  if (this.temperature >= MAXIMUM) {
+    throw new Error("Maximum Temperature!")
+  };
   this.temperature += INCREASE;
 }
 
 Thermostat.prototype.down = function() {
+  if (this.temperature <= MINIMUM) {
+    throw new Error("Minimum Temperature!");
+  };
   this.temperature -= DECREASE;
 }
