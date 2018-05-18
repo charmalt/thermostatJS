8c12e6b02b68eb2bb1a3e5a48f7295bb8732cb27
Charlene <charlenechetcuti@gmail.com>
2018-05-18 15:20:41 +0100

    styling

--- thermostatFunctions.js
+++ thermostatFunctions.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   $('#city-form').submit(function(){
     event.preventDefault();
     var cityName = $('#city').val();
+    $("#city-name").text(cityName);
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&APPID=06d5aa42080a1c3d436218f627f29502', function (response){
       $("#city-temperature").text(response.main.temp);
     });
@@ -12,31 +13,31 @@ $(document).ready(function(){
   });
 
   $(".up").click(function(){
-    $(".errormessages").text('Everything is working fine!');
+    $(".errormessages").text('Jack is safe.');
     try {
       thermostat.up();
     }
     catch (error) {
-      $(".errormessages").text('Maximum Temperature Reached!');
+      $(".errormessages").text('It\'s getting hot in here.');
     }
     var temperature = thermostat.temperature;
     $(".temperature").text(temperature);
   });
 
   $(".down").click(function(){
-    $(".errormessages").text('Everything is working fine!');
+    $(".errormessages").text('Jack is safe.');
     try {
       thermostat.down();
     }
     catch (error) {
-      $(".errormessages").text('Minimum Temperature Reached!');
+      $(".errormessages").text('Jack is frozen.');
     }
     var temperature = thermostat.temperature;
     $(".temperature").text(temperature);
   });
 
   $(".reset").click(function(){
-    $(".errormessages").text('Everything is working fine!');
+    $(".errormessages").text('Jack is safe.');
     thermostat.reset();
     var temperature = thermostat.temperature;
   $(".temperature").text(temperature);
