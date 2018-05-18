345419c0f6f508eefb2378d832e4d89a267dca36
Charlene <charlenechetcuti@gmail.com>
2018-05-18 09:51:32 +0100

    refactor + add css

--- thermostatFunctions.js
+++ thermostatFunctions.js
@@ -3,10 +3,44 @@ $(document).ready(function(){
   var temperature = thermostat.temperature;
 
   $(".up").click(function(){
-    thermostat.up();
+    $(".errormessages").text('Everything is working fine!');
+    try {
+      thermostat.up();
+    }
+    catch (error) {
+      $(".errormessages").text('Maximum Temperature Reached!');
+    }
+    var temperature = thermostat.temperature;
+    $(".temperature").text(temperature);
+  });
+
+  $(".down").click(function(){
+    $(".errormessages").text('Everything is working fine!');
+    try {
+      thermostat.down();
+    }
+    catch (error) {
+      $(".errormessages").text('Minimum Temperature Reached!');
+    }
+    var temperature = thermostat.temperature;
+    $(".temperature").text(temperature);
+  });
+
+  $(".reset").click(function(){
+    $(".errormessages").text('Everything is working fine!');
+    thermostat.reset();
+    var temperature = thermostat.temperature;
+  $(".temperature").text(temperature);
+  });
+
+  $(".switch").click(function(){
+    thermostat.powerSavingOff();
+    thermostat.reset();
     var temperature = thermostat.temperature;
   $(".temperature").text(temperature);
   });
 
   $(".temperature").text(temperature);
+
+  $("#sw").prop('checked', true);
 });
